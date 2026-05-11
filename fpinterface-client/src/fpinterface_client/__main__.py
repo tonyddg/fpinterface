e82d6e828f633d1240584a1d8642620bd03c475b
get <799052200@qq.com>
2026-05-11 20:38:35 +0800

    fix: 修复 fpinterface-client 示例图片没有打包到包中的问题 feat: 将 fpinterface-client 中可视化相关包分离为 cli 的 feat: 优化 __main__ 中的命令行参数逻辑

--- fpinterface-client/src/fpinterface_client/__main__.py
+++ fpinterface-client/src/fpinterface_client/__main__.py
@@ -1,66 +1,92 @@
-from .client import FoundationPoseClient
-from .utility import depth_filter
-from .visualize import *
+from typing import Optional
+
+from fpinterface_client.client import FoundationPoseClient
+from fpinterface_client.utility import depth_filter
+from fpinterface_client.visualize import draw_mesh_axis_bbox
 
 if __name__ == "__main__":
 
     from pathlib import Path
     from dataclasses import dataclass
+    from importlib.resources import files, as_file
 
     try:
         from matplotlib import pyplot as plt
         import tyro
     except:
-        raise RuntimeError("运行示例前请先安装 tyro 与 matplotlib")
-    
-    app_dir = Path(__file__).parent.joinpath("../../")
+        raise RuntimeError("运行示例前请通过 pip install ...[cli] 或 uv sync --all-extras 安装开发依赖 tyro 与 matplotlib")
+
     @dataclass
     class CliArgs:
-        # RGB 文件路径
-        COLOR_IMG: str = app_dir.joinpath("example/color.png").as_posix()
-        # 掩膜图片路径
-        MASK_IMG: str = app_dir.joinpath("example/workspace_mask.png").as_posix()
-        # 深度图片路径
-        DEPTH_IMG: str = app_dir.joinpath("example/depth.png").as_posix()
+
+        @dataclass
+        class ImgPath:
+            ''' 图片路径, 不给出时使用示例图片'''
+            # RGB 文件路径
+            color: str
+            # 掩膜图片路径
+            mask: str
+            # 深度图片路径
+            depth: str
+
+        # 图片路径, 不给出时使用示例图片
+        src: Optional[ImgPath] = None
         # 是否绘制原始坐标系
-        IS_DRAW_ORIN_POSE: bool = False
+        is_draw_orin_pose: bool = False
         # 绘制目标
-        TARGET_LABEL: str = "blue_big"
+        target_label: str = "blue_big"
     config = tyro.cli(CliArgs)
 
     import cv2
     import time
 
-    rgb = cv2.cvtColor(cv2.imread(config.COLOR_IMG), cv2.COLOR_BGR2RGB) # 用于分析
-    # 来自 YcbineoatReader.get_mask
-    mask = cv2.imread(config.MASK_IMG, -1).astype(bool)
-    # 获取深度图
-    depth = cv2.imread(config.DEPTH_IMG, -1) / 1e3
+    if (config.src is None):
+        
+        # 加载示例图片
+        
+        example_color_img = files("fpinterface_client.assets").joinpath("color.png")
+        example_mask_img = files("fpinterface_client.assets").joinpath("workspace_mask.png")
+        example_depth_img = files("fpinterface_client.assets").joinpath("depth.png")
+
+        with as_file(example_color_img) as example_color_src:
+            rgb = cv2.cvtColor(cv2.imread(example_color_src.as_posix()), cv2.COLOR_BGR2RGB)
+        with as_file(example_mask_img) as example_mask_src:
+            mask = cv2.imread(example_mask_src.as_posix(), -1).astype(bool)
+        with as_file(example_depth_img) as example_depth_src:
+            depth = cv2.imread(example_depth_src.as_posix(), -1) / 1e3
+
+    else:
+        rgb = cv2.cvtColor(cv2.imread(config.src.color), cv2.COLOR_BGR2RGB) # 用于分析
+        # 来自 YcbineoatReader.get_mask
+        mask = cv2.imread(config.src.mask, -1).astype(bool)
+        # 获取深度图
+        depth = cv2.imread(config.src.depth, -1) / 1e3
+
     depth = depth_filter(depth)
 
     fpc = FoundationPoseClient()
 
     start_time = time.perf_counter()
     bbox_pose = fpc.infer(
-        config.TARGET_LABEL,
+        config.target_label,
         rgb, depth, mask, is_bbox_pose = True
     )
     use_time = time.perf_counter() - start_time
     print(f"use time: {use_time:.3f}s")
     print(bbox_pose)
 
-    if config.IS_DRAW_ORIN_POSE:
+    if config.is_draw_orin_pose:
         origin_pose = fpc.infer(
-            config.TARGET_LABEL,
+            config.target_label,
             rgb, depth, mask, is_bbox_pose = False
         )
     else: 
         origin_pose = None
 
     bbox_rgb = rgb.copy()
-    bbox_min_xyz, bbox_max_xyz = fpc.get_bbox(config.TARGET_LABEL) 
+    bbox_min_xyz, bbox_max_xyz = fpc.get_bbox(config.target_label) 
     bbox_rgb = draw_mesh_axis_bbox(
-        bbox_rgb, bbox_pose, fpc.cam_k, bbox_min_xyz=bbox_min_xyz, bbox_max_xyz = bbox_max_xyz, label = config.TARGET_LABEL, origin_pose = origin_pose
+        bbox_rgb, bbox_pose, fpc.cam_k, bbox_min_xyz=bbox_min_xyz, bbox_max_xyz = bbox_max_xyz, label = config.target_label, origin_pose = origin_pose
     )
 
     mask_rgb = rgb.copy()
